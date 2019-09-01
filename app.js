1e701f0089206232afa91fc9dcc89173131d0b33
George Véras Valentim <george.veras.valentim@gmail.com>
2019-08-31 21:55:36 -0300

    Experimenting

--- app.js
+++ app.js
@@ -2,11 +2,25 @@ const app = require('http').createServer()
 const io = require('socket.io')(app)
 const port = 3001
 
+let handshakes = {
+  'handshakeId': {
+    offerFromP1: {},
+    answerFromP2: {},
+    candidatesToP1: '',
+    candidatesToP2: '',
+    status: 'completed'
+  }
+}
+
 io.on('connection', function(socket) {
   console.log('A client is connected!')
 
   socket.on('candidate', data => {
     console.log(data)
+  })
+
+  socket.on('handshakeRequest', data => {
+    socket.emit('id', "handshakeId")
   })
 })
 
